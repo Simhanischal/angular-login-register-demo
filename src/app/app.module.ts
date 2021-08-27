77e85037fef6e56523ed43568b38dbca34c2bd8d
Simha S <nischal.simha.s@accenture.com>
2021-08-28 00:11:46 +0530

    Added reactive form controls with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
 import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,8 +18,10 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatIconModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, Validators, } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -6,18 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private toastr: ToastrService) { }
 
   ngOnInit(): void {  }
 
-  login = true;
+  login: boolean = true;
 
-  goToRegister(): void {
-    this.login = false;
+  //register section
+  registerForm = this.fb.group({
+    username: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  get regControl(){
+    return this.registerForm.controls;
+  }
+
+  onRegister(): void{
+    if(!this.registerForm.valid)
+      this.toastr.error('Please correct the issues in the form');
+    else
+    this.toastr.success('User registered successfully');
   }
 
   goToLogin(): void {
     this.login = true;
+  }
+
+  //login section
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  get logControl(){
+    return this.loginForm.controls;
+  }
+
+  onLogin(): void{
+    if(!this.loginForm.valid)
+      this.toastr.error('Please correct the issues in the form');
+    else
+    this.toastr.success('User logged in successfully');
+  }
+
+  goToRegister(): void {
+    this.login = false;
   }
 
 }
